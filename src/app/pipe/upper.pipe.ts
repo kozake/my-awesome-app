443f756ac5a2eb6c17c182748fe2219bbf2461ee
kozake <sh.kozake@gmail.com>
2017-02-11 13:00:48 +0900

    add upper logic.

--- src/app/pipe/upper.pipe.ts
+++ src/app/pipe/upper.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class UpperPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: string, args?: any): string {
+    return value.toUpperCase();
   }
 
 }
