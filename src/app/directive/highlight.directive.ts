bc4e02ef0130f27ddeb258f5ffe3b5dd0b96f492
kozake <sh.kozake@gmail.com>
2017-02-11 11:39:37 +0900

    highlight if nice

--- src/app/directive/highlight.directive.ts
+++ src/app/directive/highlight.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  @Input()
+  set appHighlight(condition: any) {
+    if (condition) {
+      this.el.nativeElement.style.background = 'yellow';
+    } else {
+      this.el.nativeElement.style.background = null;
+    }
+  }
+
+  constructor(private el: ElementRef) {
+  }
 
 }
