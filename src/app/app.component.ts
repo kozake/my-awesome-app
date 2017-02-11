5e1886743f14d8d7932436ae2feb32ef689d380f
kozake <sh.kozake@gmail.com>
2017-02-11 10:18:21 +0900

    myFavoriteBeer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Beer } from './beer/beer.model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello World!';
+  myFavoriteBeer = new Beer('1', 'モルツ', '最高だ');
 }
---
+++ src/app/beer/beer.model.ts
@@ -0,0 +1,8 @@
+export class Beer {
+
+    constructor(
+        private id: string,
+        private name: string,
+        private description: string) {
+        }
+}
